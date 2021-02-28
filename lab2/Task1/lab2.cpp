a49baa647ee758cfb146f089611496a6ed340442
Tanya Grunicheva <eiforiaa.2010@mail.ru>
2021-02-28 13:25:29 +0300

    second task

---
+++ lab2/Task1/lab2.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+double x, y;
+
+bool isEqual(double x, double y)
+{
+    return ((x >= 0 != 0 && x <= 1 != 0 && y >= 0 != 0 && y <= 1 != 0 && x*x+y*y <=1 )||(x <= 0 != 0 && x>= -1 && y <= 0 && y >= -1));; // оператор == возвращает true, если x равно y, в противном случае - false 
+}
+
+int main()
+{
+    cin >> x >> y;
+    bool a = isEqual(x, y);
+   /* bool res = ((x >= 0 != 0 && x <= 1 != 0 && y >= 0 != 0 && y <= 1 != 0 && x*x+y*y <=1 )||(x <= 0 != 0 && x>= -1 && y <= 0 && y >= -1));*/
+    cout << a;
+  
+
+    return 0;
+}
+
---
+++ lab2/Task2/Task2.cpp
@@ -0,0 +1,24 @@
+﻿// Task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+using namespace std;
+#include <iostream>
+double res, x;
+double f(double x) {
+    cin >> x;
+    if (x <= 3) 
+        res = x * x - 3 * x + 9;
+    else 
+       res =  1 / (x * x * x + 6);
+    
+    return res;
+}
+
+int main()
+{
+    double m = f(x);
+    cout << m;
+    
+}
+
+
