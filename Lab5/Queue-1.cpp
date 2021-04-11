ea9f25332ee380a0e8c6900c6490f6220d69049e
Tanya Grunicheva <eiforiaa.2010@mail.ru>
2021-04-12 01:45:50 +0300

    5lab

---
+++ Lab5/Queue-1.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct Node
+{
+    int x;
+    Node* Next;
+};
+class List
+{
+    Node* Head, * Tail; //Указатели на начало списка и на конец
+public:
+    List() :Head(NULL), Tail(NULL) {}; //Инициализация указателей как пустых
+    ~List(); //Деструктор для освобождения памяти от всего что будет
+    void Push(int x); //Функция заполнения структуры Node и добавления её в список как элемента
+    void Peek();//Функция отображения крайнего элемента
+    void Pop();//Удаление первого элемента
+    friend ostream& operator << (ostream& stream, const List& a); // Перегрузка оператора вывода
+};
+List::~List() //ДЕСТРУКТОР ДЛЯ ОЧИСТКИ ПАМЯТИ
+{
+    Node* temp = Head; //Временный указатель на начало списка
+    while (temp != 0) //Пока в списке что-то есть
+    {
+        temp = Head->Next; //Резерв адреса на следующий элемент списка
+        delete Head;  //Освобождение памяти от первой структуры как элемента списка
+        Head = temp; //Сдвиг начала на следующий адрес, который берем из резерва
+    }
+}
+void List::Push(int x)
+{
+    Node* temp = new Node; //Выделение памяти для нового звена списка
+    temp->x = x; //Временное запоминание принятого параметра x
+    temp->Next = NULL; //Указание, что следующее звено новосозданной структуры пока пустое
+
+    if (Head != NULL) //Если список не пуст 
+    {
+        Tail->Next = temp; //Указание, что следующее звено списка это новосозданная структура
+        Tail = temp;
+    }
+    else Head = Tail = temp; //Если список не пуст, добавление первого элемента
+}
+ostream& operator << (ostream& stream, const List& a)
+{
+    Node* temp = a.Head; //Временный указатель на начало списка
+    while (temp != NULL) //Пока в списке что-то встречается
+    {
+        stream << temp->x << ""; //Выводим значения из списка на экран
+        temp = temp->Next; //Сдвигаем указатель на начало на адрес следующего элемента
+    }
+    return stream << endl;
+}
+void List::Peek()
+{
+    cout << Head->x;
+}
+void List::Pop()
+{
+    Head = Head->Next;
+}
+void main()
+{
+    List lst;
+    int size, count;
+    cout << "Size = ";
+    cin >> size;
+    for (int i = 1; i <= size; i++)
+    {
+        cout << "Count " << i << "/" << size << " = ";
+        cin >> count;
+        lst.Push(count);
+    }
+    cout << lst;
+    lst.Peek();
+    lst.Pop();
+    cout << endl;
+    cout << lst;
+}
+
